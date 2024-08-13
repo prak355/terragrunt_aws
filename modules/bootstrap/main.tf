1ddf2f6ab219f77be7d5c6c4f462aa005b20f6e1
Prakash Patel <prak355@gmail.com>
2024-08-13 18:17:22 +0100

    added bootstrap module files

--- modules/bootstrap/main.tf
+++ modules/bootstrap/main.tf
@@ -1,1 +1,37 @@
-# Placeholder for modules/bootstrap/main.tf
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.s3_bucket_name
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name           = var.dynamodb_table_name
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = var.tags
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.terraform_state.bucket
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+}
--- modules/bootstrap/variables.tf
+++ modules/bootstrap/variables.tf
@@ -1,1 +1,16 @@
-# Placeholder for modules/bootstrap/variables.tf
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to store Terraform state"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table to use for Terraform state locking"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resources"
+  type        = map(string)
+  default     = {}
+}
+
